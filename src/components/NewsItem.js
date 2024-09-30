dc44871d9633517be820017160dc4481d0a671f1
Datta <patil.sk2023@gmail.com>
2024-09-30 18:12:00 +0530

    src file

---
+++ src/components/NewsItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <img src={imageUrl?imageUrl:"https://plus.unsplash.com/premium_photo-1707080369554-359143c6aa0b?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
